5047a7ad687c8cf7b37825e728d44250146f7b46
ALEXANDRE LORENA <138406816+alexandrelorena@users.noreply.github.com>
2024-03-13 22:54:23 -0300

    Indentação corrigida em super_pecas_tables.sql

--- semana1/scripts/super_pecas_tables.sql
+++ semana1/scripts/super_pecas_tables.sql
@@ -3,16 +3,16 @@ CREATE DATABASE SuperPecas;
 USE SuperPecas;
 
 CREATE TABLE IF NOT EXISTS carros (
-	id INT NOT NULL AUTO_INCREMENT,
-	nome_do_modelo VARCHAR(45) NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    nome_do_modelo VARCHAR(45) NULL,
     fabricante VARCHAR(45) NULL,
     codigo_unico varchar(45) NULL,
     PRIMARY KEY (id),
-	UNIQUE INDEX `codigo_unico_UNIQUE` (codigo_unico)
+    UNIQUE INDEX `codigo_unico_UNIQUE` (codigo_unico)
 );
 
 CREATE TABLE IF NOT EXISTS pecas (
-	id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(45) NULL,
     descricao VARCHAR(45) NULL,
     numero_de_serie VARCHAR(45) NULL,
@@ -22,5 +22,5 @@ CREATE TABLE IF NOT EXISTS pecas (
     PRIMARY KEY (id),
     UNIQUE INDEX `numero_de_serie_UNIQUE` (numero_de_serie),
     CONSTRAINT fk_pecas_carros
-	FOREIGN KEY (carro_id) REFERENCES carros(id)    
+    FOREIGN KEY (carro_id) REFERENCES carros(id)    
 );
