fa46f3f3f281db52c1a52c3fd0e66a05361f3724
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-03-14 00:30:02 -0300

    Projeto no Spring Initializr

---
+++ superpecas/src/main/java/br/com/masterclass/superpecas/SuperPecasApplication.java
@@ -0,0 +1,13 @@
+package br.com.masterclass.superpecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperPecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperPecasApplication.class, args);
+	}
+
+}
