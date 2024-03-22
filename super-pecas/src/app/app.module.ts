a27dda962a243cc1a57084e03295854879ba3ea3
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-03-22 19:33:55 -0300

    atualizações

--- super-pecas/src/app/app.module.ts
+++ super-pecas/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { CarrosComponent } from './carros/carros.component';
 import { PecasComponent } from './pecas/pecas.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HeaderComponent } from './header/header.component';
 import { TemplateComponent } from './template/template.component';
-import { HttpClientModule } from '@angular/common/http';
 import { ContentComponent } from './content/content.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -35,12 +37,14 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes), HttpClientModule,
-    MatSidenavModule
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatInputModule,
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
