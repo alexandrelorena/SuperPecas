8e96be51bd16d0bab1a14a7e4e6d8d3b3626e0fd
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-03-23 13:28:55 -0300

    atualização rotas

--- super-pecas/src/app/app-routing.module.ts
+++ super-pecas/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
+import { HomeComponent } from './home/home.component';
 import { CarrosComponent } from './carros/carros.component';
 import { PecasComponent } from './pecas/pecas.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'carros', component: CarrosComponent },
   { path: 'pecas', component: PecasComponent },
-  { path: '', redirectTo: '/carros', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 
 ];
 
